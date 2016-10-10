191a0ed5ad423422d2535d9187d59be1f3e83488
Timofey Kirillov <distorhead@gmail.com>
2016-10-10 14:41:08 +0300

    ++

--- lib/dockerlib/remote/client.rb
+++ lib/dockerlib/remote/client.rb
@@ -8,7 +8,7 @@ module Dockerlib
       def inspect_container(name)
         resp = connection.request(method: :get, path: "/containers/#{name}/json")
         if resp.status == 200
-          {d: load_body(resp)}
+          {data: load_body(resp)}
         elsif resp.status == 404
           {code: :no_such_container}
         else
@@ -27,8 +27,8 @@ module Dockerlib
       end
 
       def to_server_error(resp)
-        {error: :server_error, d: {http_status: resp.status,
-                                   http_body: resp.body}}
+        {error: :server_error, data: {http_status: resp.status,
+                                      http_body: resp.body}}
       end
     end # Client
   end # Remote
